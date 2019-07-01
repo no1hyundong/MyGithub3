f1cc946611dad68434c93807280998c2d32896f1
fytuhaibo <no1hyundong@gmail.com>
2019-07-01 16:10:43 +0900

    Initial Commit

---
+++ MyGithub3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyGithub3
+//
+//  Created by TJ on 01/07/2019.
+//  Copyright © 2019 Kurhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
